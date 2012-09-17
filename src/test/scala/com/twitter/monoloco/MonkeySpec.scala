5de41895da70e5ff20cde96959dda9acdf32cf89
Julio Capote <julio@twitter.com>
2012-09-16 20:35:57 -0700

    fixed test

--- src/test/scala/com/twitter/monoloco/MonkeySpec.scala
+++ src/test/scala/com/twitter/monoloco/MonkeySpec.scala
@@ -2,26 +2,29 @@ package com.twitter.monoloco
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.FunSpec
+import org.scalatest.{FlatSpec, FunSpec}
 import org.scalatest.mock.MockitoSugar
 import org.mockito.Mockito._
-import org.scalatest.matchers.MustMatchers
+import org.scalatest.matchers.{ShouldMatchers, MustMatchers}
+import com.twitter.logging.{Logger, LoggerFactory, FileHandler}
+import com.twitter.logging.config._
+import scala.Some
 
 @RunWith(classOf[JUnitRunner])
-class MonkeySpec extends FunSpec with MockitoSugar with MustMatchers {
+class MonkeySpec extends FlatSpec with MockitoSugar with ShouldMatchers {
 
-   describe("DefaultMonkey doesn't accept an empty Trick List") {
-      intercept[IllegalArgumentException] {
-         new DefaultMonkey(List())
-      }
+  "DefaultMonkey" should "not accept an empty Trick List" in  {
+    intercept[IllegalArgumentException] {
+      new DefaultMonkey(List())
     }
-    
-    describe("one call only once") {
-      val trickMock = mock[Trick]      
-      
-      val monkey = new DefaultMonkey(List(trickMock))
-      monkey.apply()
-            
-      verify(trickMock).execute()
-    }
+  }
+
+  "DefaultMonkey" should "only call once" in {
+    val trickMock = mock[Trick]
+
+    val monkey = new DefaultMonkey(List(trickMock))
+    monkey.apply()
+
+    verify(trickMock).apply()
+  }
 }
--- src/test/scala/com/twitter/monoloco/TrickSpec.scala
+++ src/test/scala/com/twitter/monoloco/TrickSpec.scala
@@ -2,16 +2,14 @@ package com.twitter.monoloco.test
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.FunSpec
-import org.scalatest.matchers.MustMatchers
+import org.scalatest.{FlatSpec, FunSpec}
+import org.scalatest.matchers.{ShouldMatchers, MustMatchers}
 
 @RunWith(classOf[JUnitRunner])
-class TrickSpec extends FunSpec with MustMatchers {
+class TrickSpec extends FlatSpec with ShouldMatchers {
 
-  describe("trick test") {
-    it("should execute") {
-      "2" must equal("2")
-    }
+  "trick test" should "work" in {
+      "2" should equal("2")
   }
 
 }
